a9ea86c92261168282e1900e04051c734954c85a
Renato Tales <137285295+renatotales3@users.noreply.github.com>
2025-08-20 23:42:36 -0300

    Create typography.ts

---
+++ src/app/theme/typography.ts
@@ -0,0 +1,8 @@
+import { TextStyle } from 'react-native';
+
+export const typography: { [key: string]: TextStyle } = {
+  h1: { fontFamily: 'Inter_700Bold', fontSize: 32 },
+  h2: { fontFamily: 'Inter_600SemiBold', fontSize: 24 },
+  body: { fontFamily: 'Inter_400Regular', fontSize: 16 },
+  caption: { fontFamily: 'Inter_400Regular', fontSize: 12 },
+};
